83fb4dee33bed26c802ad8a30831a8864d5fe0ff
elpe2005 <Epedersen2005@gmail.com>
2022-10-25 18:27:15 +0200

    Add project files.

---
+++ uppgift4.26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace uppgift424
+{   
+    class program
+    {
+        static void Main(string[]args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
